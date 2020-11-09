e666fd8834949b4921c70513779748c0aa1b6b88
franco7scala <franco7scala@gmail.com>
2020-11-09 22:19:32 +0100

    completed home

--- lib/UI/pages/Home.dart
+++ lib/UI/pages/Home.dart
@@ -1,4 +1,3 @@
-import 'package:RendicontationPlatformLeo_Client/UI/aspects/LeoTextStyles.dart';
 import 'package:RendicontationPlatformLeo_Client/UI/aspects/UIConstants.dart';
 import 'package:RendicontationPlatformLeo_Client/UI/behaviors/AppLocalizations.dart';
 import 'package:RendicontationPlatformLeo_Client/UI/behaviors/GlobalState.dart';
@@ -10,10 +9,8 @@ import 'package:RendicontationPlatformLeo_Client/model/ModelFacade.dart';
 import 'package:RendicontationPlatformLeo_Client/model/objects/Club.dart';
 import 'package:RendicontationPlatformLeo_Client/model/support/Constants.dart';
 import 'package:RendicontationPlatformLeo_Client/model/support/StringCapitalization.dart';
-
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
+import 'package:flutter/material.dart';
 
 
 class Home extends StatefulWidget {
@@ -61,7 +58,7 @@ class _Home extends GlobalState<Home> {
           child: Column(
             children: [
               Padding(
-                padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
                 child: Text(
                   AppLocalizations.of(context).translate("hi").capitalize + "!",
                   style: TextStyle(
@@ -75,61 +72,65 @@ class _Home extends GlobalState<Home> {
                   width: 200,
                 ),
                 colorFilter: ColorFilter.mode(
-                    Theme.of(context).splashColor, BlendMode.modulate
+                    Theme.of(context).splashColor,
+                    BlendMode.modulate
+                ),
+              ),
+              Text(
+                club.name,
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+              Text(
+                AppLocalizations.of(context).translate("district").capitalize + " " + club.district.name,
+                style: TextStyle(
+                  fontSize: 17,
                 ),
               ),
               Padding(
-                padding: EdgeInsets.fromLTRB(0, 0, 0, 30),
+                padding: EdgeInsets.fromLTRB(0, 3, 0, 25),
                 child: Text(
-                  club.name,
+                  AppLocalizations.of(context).translate("since").capitalize + " " + club.foundationDate.year.toString(),
                   style: TextStyle(
-                    fontSize: 25,
+                    fontSize: 12,
                   ),
                 ),
               ),
-              StadiumButton(
-                icon: UIConstants.ICON_SERVICE,
-                title: AppLocalizations.of(context).translate("service"),
-                onPressed: () {
-                  Navigator.of(context).push(
-                    PageRouteBuilder(
-                      opaque: false,
-                      transitionDuration: Duration(milliseconds: 700),
-                      pageBuilder: (BuildContext context, _, __) => Services()
-                    ),
-                  );
-                },
-              ),
-              StadiumButton(
-                icon: UIConstants.ICON_ACTIVITY,
-                title: AppLocalizations.of(context).translate("activity"),
-                onPressed: () {
-                  Navigator.of(context).push(
-                    PageRouteBuilder(
-                      opaque: false,
-                      transitionDuration: Duration(milliseconds: 700),
-                      pageBuilder: (BuildContext context, _, __) => Activities()
-                    ),
-                  );
-                },
-              ),
-
-
-
-
               Padding(
-                padding: EdgeInsets.all(10),
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 25),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    StadiumButton(
+                      icon: UIConstants.ICON_SERVICE,
+                      title: AppLocalizations.of(context).translate("service").capitalize,
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          PageRouteBuilder(
+                              opaque: false,
+                              transitionDuration: Duration(milliseconds: 700),
+                              pageBuilder: (BuildContext context, _, __) => Services()
+                          ),
+                        );
+                      },
+                    ),
+                    StadiumButton(
+                      icon: UIConstants.ICON_ACTIVITY,
+                      title: AppLocalizations.of(context).translate("activity").capitalize,
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          PageRouteBuilder(
+                              opaque: false,
+                              transitionDuration: Duration(milliseconds: 700),
+                              pageBuilder: (BuildContext context, _, __) => Activities()
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
               ),
-              club == null ? Text("no club") : Column(
-                children: [
-                  Text("name: " + club.name + " email: " + club.email + " district: " + club.district.name),
-                  Text("quantity services: " + club.quantityServices.toString()),
-                  Text("quantity activities: " + club.quantityActivities.toString()),
-                ],
-              ),
-
-
-
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
@@ -137,6 +138,13 @@ class _Home extends GlobalState<Home> {
                     padding: EdgeInsets.all(5),
                     child: Column(
                       children: [
+                        Text(
+                          AppLocalizations.of(context).translate("service").capitalize,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        ),
                         Container(
                           padding: EdgeInsets.all(5),
                           width: 100,
@@ -144,14 +152,17 @@ class _Home extends GlobalState<Home> {
                           child: LiquidCircularProgressIndicator(
                             value: (club.quantityServices.currentYear / 15),
                             valueColor: AlwaysStoppedAnimation(Colors.greenAccent),
-                            backgroundColor: Colors.white,
+                            backgroundColor: Theme.of(context).primaryColor,
                             borderColor: Theme.of(context).buttonColor,
                             borderWidth: 3.0,
                             direction: Axis.vertical,
                           ),
                         ),
                         Text(
-                            "Services questo anno " + club.quantityServices.currentYear.toString()
+                            AppLocalizations.of(context).translate("this_year") + " " + club.quantityServices.currentYear.toString()
+                        ),
+                        Text(
+                            AppLocalizations.of(context).translate("since_the_foundation") + " " + (club.quantityServices.currentYear + ( DateTime.now().year - club.foundationDate.year) * 15).toString()
                         ),
                       ],
                     ),
@@ -160,33 +171,39 @@ class _Home extends GlobalState<Home> {
                     padding: EdgeInsets.all(5),
                     child: Column(
                       children: [
+                        Text(
+                          AppLocalizations.of(context).translate("activity").capitalize,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        ),
                         Container(
                           padding: EdgeInsets.all(5),
                           width: 100,
                           height: 100,
                           child: LiquidCircularProgressIndicator(
-                              value: (club.quantityActivities.currentYear / 15),
+                              value: (club.quantityActivities.currentYear / 17),
                             valueColor: AlwaysStoppedAnimation(Colors.blue),
-                            backgroundColor: Colors.white,
+                            backgroundColor: Theme.of(context).primaryColor,
                             borderColor: Theme.of(context).buttonColor,
                             borderWidth: 3.0,
                             direction: Axis.vertical,
                           ),
                         ),
                         Text(
-                            "Attività questo anno " + club.quantityActivities.currentYear.toString()
+                            AppLocalizations.of(context).translate("this_year") + " " + club.quantityActivities.currentYear.toString()
+                        ),
+                        Text(
+                            AppLocalizations.of(context).translate("since_the_foundation") + " " + (club.quantityActivities.currentYear + ( DateTime.now().year - club.foundationDate.year) * 17).toString()
                         ),
                       ],
                     ),
                   ),
                 ],
               ),
-
-
-
-
               Padding (
-                padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
+                padding: EdgeInsets.fromLTRB(0, 25, 0, 20),
                 child: FlatButton(
                   minWidth: 100000,
                   color: Colors.transparent,
@@ -215,5 +232,6 @@ class _Home extends GlobalState<Home> {
       ),
     );
   }
+
 
 }
