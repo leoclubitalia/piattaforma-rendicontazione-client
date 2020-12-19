5113040d6870ae43a2d7c5ea975624858caf3e97
franco7scala <franco7scala@gmail.com>
2020-12-19 22:40:46 +0100

    some order

--- lib/Main.dart
+++ lib/Main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:RendicontationPlatformLeo_Client/UI/pages/rendicontation/App.dart';
+import 'package:RendicontationPlatformLeo_Client/UI/App.dart';
 
 
 void main() => runApp(App());
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:RendicontationPlatformLeo_Client/UI/pages/rendicontation/App.dart';
+import 'package:RendicontationPlatformLeo_Client/UI/App.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
